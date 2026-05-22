03670b4689e01b538009e7c1e58265eed0ebed96
benjaminnayottama-design <benjaminnayottama@gmail.com>
2026-05-22 23:05:35 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Sistem Report Siswa berjalan dengan baik.");
+console.log("Tema Dark Mode aktif.");
+
+function FuctionJS() {
+    alert("Selamat datang di website profile saya. Jangan lupa cek project yang sudah saya buat di bawah ya. Terimakasih banyak.");
+}
